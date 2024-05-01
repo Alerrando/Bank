2fbcba267a7a9f464f7c6b431c21e7c5c362c022
Alerrando <alerrando2@gmail.com>
2024-05-01 14:32:35 -0300

    feat: add folder security and JWTConfiguration SecurityConfiguration and SecurityFilter

--- server/src/main/java/com/bank/repositories/UserRepository.java
+++ server/src/main/java/com/bank/repositories/UserRepository.java
@@ -4,9 +4,13 @@ import com.bank.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByEmail(@Param("email") String email);
+
+    @Query("SELECT u FROM User u WHERE u.email = :email")
+    UserDetails findUser(@Param("email") String email);
 }
---
+++ server/src/main/java/com/bank/security/SecurityFilter.java
@@ -0,0 +1,53 @@
+package com.bank.security;
+
+import com.auth0.jwt.algorithms.Algorithm;
+import com.bank.repositories.UserRepository;
+import com.bank.services.TokenService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+    @Autowired
+    TokenService tokenService;
+
+    @Autowired
+    UserRepository userRepository;
+
+    @Autowired
+    private Algorithm algorithm; // Definir o algoritmo de assinatura
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if (cookie.getName().equals("accessToken")) {
+                    String token = cookie.getValue();
+                    var subject = tokenService.validateToken(token);
+                    UserDetails user = userRepository.findUser(subject);
+                    if(user != null){
+                        var authentication = new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                    }
+                }
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
