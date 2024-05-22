e145d14b4876b596e02607537f8d612ad3309753
Alerrando <alerrando2@gmail.com>
2024-05-22 10:50:44 -0300

    feat: add Suspense in Layout

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { cn } from "@/lib/utils";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -23,7 +23,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={cn("min-h-screen bg-background font-sans antialiased", fontSans.variable)}>
-        <StateProvider>{children}</StateProvider>
+        <StateProvider>
+          <Suspense>{children}</Suspense>
+        </StateProvider>
       </body>
     </html>
   );
