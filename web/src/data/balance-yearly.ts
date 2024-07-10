6ce0b1c63986415455bc2889434f4a5595e41c25
Alerrando <alerrando2@gmail.com>
2024-07-09 23:48:18 -0300

    feat: add balance-yearly with fake data for balance yearly

---
+++ web/src/data/balance-yearly.ts
@@ -0,0 +1,80 @@
+type BalanceYearlyData = {
+  month: string;
+  deposits: number;
+  withdrawals: number;
+};
+
+export const balanceYearlyData: BalanceYearlyData[] = [
+  {
+    name: "Jan",
+    deposits: 150,
+    withdrawals: 2400,
+    total: 2400,
+  },
+  {
+    name: "Feb",
+    deposits: 360,
+    withdrawals: 1398,
+    total: 2210,
+  },
+  {
+    name: "Mar",
+    deposits: 2000,
+    withdrawals: 9800,
+    total: 2290,
+  },
+  {
+    name: "Apr",
+    deposits: 2780,
+    withdrawals: 3908,
+    total: 2000,
+  },
+  {
+    name: "May",
+    deposits: 1890,
+    withdrawals: 4800,
+    total: 2181,
+  },
+  {
+    name: "Jun",
+    deposits: 2390,
+    withdrawals: 3800,
+    total: 2500,
+  },
+  {
+    name: "Jul",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+  {
+    name: "Aug",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+  {
+    name: "Sep",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+  {
+    name: "Oct",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+  {
+    name: "Nov",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+  {
+    name: "Dec",
+    deposits: 3490,
+    withdrawals: 4300,
+    total: 2100,
+  },
+];
